37b2f0505222bbc5f259bc1ad6c616d9b31f37dc
keithowino <designsolutions1629@gmail.com>
2024-03-21 06:15:55 +0300

    Complete lab.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let a = 2000
+let b = 100
+
+// Function expression
+
+function divide(x, y){
+  return (x / y);
+};
+
+// Arrow functions
+
+let square = x => x * x;
+
+let add = (a, b) => a + b;
